3af01baa465a49a62246855272bb2fc2e8124a73
TrentGrooms <trentgrooms3@gmail.com>
2026-03-31 16:34:54 -0500

    updated for unit 5

---
+++ routing/app/projects/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function Projects() {
+    return (
+        <div className="bg-green-100">
+            <NavBar />
+            <h1 className="text-3xl font-bold text-center mt-8">Projects Page</h1>
+        </div>
+    );
+}
